639a875747dcb76b89b5fd0192e8e8a09f74e580
kouki888 <yioyo5039@gmail.com>
2025-05-31 23:28:21 +0800

    Update half.py

--- half.py
+++ half.py
@@ -31,7 +31,7 @@ if theme == "深色":
             background-color: #111111;
             color: white;
         }
-        h1, h2, h3, h4, h5, h6, p, span {
+        h1, h2, h3, h4, h5, h6, p {
             color: white !important;
         }
         .dataframe th, .dataframe td {
