f3aa46350f352233fa626eb5d10ac2e75bbb6ca0
kouki888 <yioyo5039@gmail.com>
2025-07-02 14:34:37 +0800

    Update app.py

--- app.py
+++ app.py
@@ -13,11 +13,6 @@ st.set_page_config(page_title="專題作業一", page_icon="📊", layout="wide"
 # ====== API 金鑰設定 ======
 load_dotenv()
 api_key = os.getenv("API_KEY")
-if api_key:
-    genai.configure(api_key=api_key)
-else:
-    st.error("找不到 API_KEY，請確認 .env 檔案存在並格式正確")
-    st.stop()
 
 # ====== 🔒 側邊欄選單 ======
 with st.sidebar:
